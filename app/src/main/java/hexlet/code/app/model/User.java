11a027b36fa542f7752d1813a222f0bb83b82cc6
Dmitry Anufriev <dm.anufriev@gmail.com>
2024-08-27 21:55:53 +0300

    Step 2. Fix checkstyle faults. Update paths to postgres DB

--- app/src/main/java/hexlet/code/app/model/User.java
+++ app/src/main/java/hexlet/code/app/model/User.java
@@ -1,12 +1,15 @@
 package hexlet.code.app.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
